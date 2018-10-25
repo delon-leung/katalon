2ae573403c2a73dbcb64e4fe4d8c06fc8ea7c06f
CNDELEU <delon.leung@cn.abb.com>
2018-10-25 15:42:54 +1000

    new project for katalon testing

---
+++ EllipsePOC/Keywords/com/abb/ellipse/User.groovy
@@ -0,0 +1,38 @@
+package com.abb.ellipse
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import internal.GlobalVariable
+
+public class User {
+	@Keyword
+	def login(){
+		WebUI.comment('Given that the user has the valid login information')
+
+		WebUI.openBrowser(GlobalVariable.G_SiteURL)
+
+		WebUI.setText(findTestObject('Page_Login/txt_UserName'), "dl3306")
+
+		WebUI.setText(findTestObject('Page_Login/txt_Password'), "")
+
+		WebUI.comment('When user logs into the system')
+
+		WebUI.click(findTestObject('Page_Login/btn_Login'))
+
+		WebUI.comment('Then user should be able to login successfully')		
+	}
+}
---
+++ EllipsePOC/Object Repository/Page_Login/txt_Quicklaunch.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txt_Quicklaunch</name>
+   <tag></tag>
+   <elementGuidId>a4e88e55-8217-4d7a-80fb-68777f907ef4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'quicklaunch']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>quicklaunch</value>
+   </webElementProperties>
+</WebElementEntity>
